de7fa7331b11cfdf3d6aceba0c5314c66975d842
willrockies <wlsn.ads@gmail.com>
2023-01-10 17:20:51 -0300

    Structuring styles files for application (ngshop+Admin)

--- apps/ngshop/src/app/pages/product-list/product-list.component.ts
+++ apps/ngshop/src/app/pages/product-list/product-list.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
     selector: "ngshop-product-list",
     templateUrl: "./product-list.component.html",
-    styleUrls: ["./product-list.component.scss"]
+
 })
 export class ProductListComponent {}
