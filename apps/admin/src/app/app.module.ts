a001670e55784c79842db387e0902b86a2f72274
willrockies <wlsn.ads@gmail.com>
2023-01-11 11:08:24 -0300

    creating the routes and show shell

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { NxWelcomeComponent } from "./nx-welcome.component";
-import { RouterModule } from "@angular/router";
-import { appRoutes } from "./app.routes";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+const ROUTES: Routes = [
+  {
+    path:'',
+    component: ShellComponent,
+    children: [
+      {
+        path:'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" })],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
+    imports: [BrowserModule, RouterModule.forRoot(ROUTES, { initialNavigation: "enabledBlocking" })],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ apps/admin/src/app/shared/shell/shell.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "admin-shell",
+    templateUrl: "./shell.component.html",
+
+})
+export class ShellComponent {}
