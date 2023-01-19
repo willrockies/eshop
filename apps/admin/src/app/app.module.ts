8a50d90f24efb8ea39e4345331db9e6de0c4354f
willrockies <wlsn.ads@gmail.com>
2023-01-18 21:16:57 -0300

    login page

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,41 +1,42 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterModule, Routes } from "@angular/router";
-import { CategoriesService } from "@bluebits/products";
-import { ConfirmationService, MessageService } from "primeng/api";
-import { ButtonModule } from "primeng/button";
-import { CardModule } from "primeng/card";
-import { ConfirmDialogModule } from "primeng/confirmdialog";
-import { InputTextModule } from "primeng/inputtext";
-import { RippleModule } from "primeng/ripple";
-import { TableModule } from "primeng/table";
-import { ToastModule } from "primeng/toast";
-import { ToolbarModule } from "primeng/toolbar";
-import { ColorPickerModule } from "primeng/colorpicker";
-import { InputNumberModule } from "primeng/inputnumber";
-import { InputTextareaModule } from "primeng/inputtextarea";
-import { InputSwitchModule } from "primeng/inputswitch";
-import { DropdownModule } from "primeng/dropdown";
-import { EditorModule } from "primeng/editor";
-import { TagModule } from "primeng/tag";
-import { InputMaskModule } from "primeng/inputmask";
-import {FieldsetModule} from 'primeng/fieldset';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesService } from '@bluebits/products';
+import { UsersModule } from '@bluebits/users';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
 
-import { AppComponent } from "./app.component";
-import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
-import { CategoriesListComponent } from "./pages/categories/categories-list/categories-list.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { ShellComponent } from "./shared/shell/shell.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { ProductsListComponent } from "./pages/products/products-list/products-list.component";
-import { ProductsFormComponent } from "./pages/products/products-form/products-form.component";
-import { UsersFormComponent } from "./pages/users/users-form/users-form.component";
-import { UsersListComponent } from "./pages/users/users-list/users-list.component";
-import { OrdersListComponent } from "./pages/orders/orders-list/orders-list.component";
-import { OrdersDetailComponent } from "./pages/orders/orders-detail/orders-detail.component";
+import { AppComponent } from './app.component';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OrdersDetailComponent } from './pages/orders/orders-detail/orders-detail.component';
+import { OrdersListComponent } from './pages/orders/orders-list/orders-list.component';
+import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { UsersFormComponent } from './pages/users/users-form/users-form.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 const UX_MODULE = [
   CardModule,
@@ -135,7 +136,8 @@ const ROUTES: Routes = [
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES, { initialNavigation: "enabledBlocking" })
+    RouterModule.forRoot(ROUTES, { initialNavigation: "enabledBlocking" }),
+    UsersModule
   ],
   providers: [CategoriesService, MessageService, ConfirmationService],
   bootstrap: [AppComponent]
--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -1,9 +1,22 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule, Route } from "@angular/router";
-import { usersRoutes } from "./lib.routes";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { LoginComponent } from './pages/login/login.component';
+
+const ROUTES: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+]
 
 @NgModule({
-    imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule.forChild(ROUTES), InputTextModule, ButtonModule, FormsModule, ReactiveFormsModule],
+  declarations: [LoginComponent]
 })
-export class UsersModule {}
+export class UsersModule { }
