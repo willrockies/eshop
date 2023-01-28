796583f6487ce069d4731014de5d51c8512d8212
willrockies <wlsn.ads@gmail.com>
2023-01-27 23:36:14 -0300

    styling the header

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -9,22 +9,23 @@ import { ProductListComponent } from "./pages/product-list/product-list.componen
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { UiModule } from "@bluebits/ui";
-import { AccordionModule } from 'primeng/accordion';
+import { AccordionModule } from "primeng/accordion";
+import { NavComponent } from "./shared/nav/nav.component";
 
 const ROUTES: Routes = [
-  {
-    path: "",
-    component: HomePageComponent
-  },
-  {
-    path: "products",
-    component: ProductListComponent
-  }
+    {
+        path: "",
+        component: HomePageComponent
+    },
+    {
+        path: "products",
+        component: ProductListComponent
+    }
 ];
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(ROUTES), UiModule, AccordionModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent, NavComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(ROUTES), UiModule, AccordionModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
