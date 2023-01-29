70a84bc6c886bf6cc73f41dbb1b3cdbf2f551c90
willrockies <wlsn.ads@gmail.com>
2023-01-28 21:52:13 -0300

    featured products item

--- libs/products/src/lib/services/products.service.ts
+++ libs/products/src/lib/services/products.service.ts
@@ -40,4 +40,8 @@ export class ProductService {
       .get<number>(`${this.apiURLProducts}/get/count`)
       .pipe(map((objectValue: any) => objectValue.productCount));
   }
+
+  getFeaturedProducts(count: number): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiURLProducts}/get/featured/${count}`)
+  }
 }
