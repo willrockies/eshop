507fd8bf741b3e80599d60052bed7d64faa548b8
willrockies <wlsn.ads@gmail.com>
2023-02-07 20:52:19 -0300

    creating the usersState

--- libs/users/src/lib/state/users.reducer.ts
+++ libs/users/src/lib/state/users.reducer.ts
@@ -1,35 +1,31 @@
-import { EntityState, EntityAdapter, createEntityAdapter } from "@ngrx/entity";
-import { createReducer, on, Action } from "@ngrx/store";
+import { createEntityAdapter, EntityAdapter } from '@ngrx/entity';
+import { Action, createReducer, on } from '@ngrx/store';
 
-import * as UsersActions from "./users.actions";
-import { UsersEntity } from "./users.models";
+import { User } from '../models/user';
+import * as UsersActions from './users.actions';
+import { UsersEntity } from './users.models';
 
 export const USERS_FEATURE_KEY = "users";
 
-export interface UsersState extends EntityState<UsersEntity> {
-    selectedId?: string | number; // which Users record has been selected
-    loaded: boolean; // has the Users list been loaded
-    error?: string | null; // last known error (if any)
+export interface UsersState {
+  user: User,
+  isAuthenticated: boolean
 }
 
 export interface UsersPartialState {
-    readonly [USERS_FEATURE_KEY]: UsersState;
+  readonly [USERS_FEATURE_KEY]: UsersState;
 }
 
-export const usersAdapter: EntityAdapter<UsersEntity> = createEntityAdapter<UsersEntity>();
+export const initialUsersState: UsersState = {
+  user: null,
+  isAuthenticated: false
+}
 
-export const initialUsersState: UsersState = usersAdapter.getInitialState({
-    // set initial required properties
-    loaded: false
-});
-
-const reducer = createReducer(
-    initialUsersState,
-    on(UsersActions.initUsers, (state) => ({ ...state, loaded: false, error: null })),
-    on(UsersActions.loadUsersSuccess, (state, { users }) => usersAdapter.setAll(users, { ...state, loaded: true })),
-    on(UsersActions.loadUsersFailure, (state, { error }) => ({ ...state, error }))
+const usersReducer = createReducer(
+  initialUsersState,
+  on(UsersActions.buildUserSession, (state) => ({ ...state }))
 );
 
-export function usersReducer(state: UsersState | undefined, action: Action) {
-    return reducer(state, action);
+export function reducer(state: UsersState | undefined, action: Action) {
+  return usersReducer(state, action);
 }
